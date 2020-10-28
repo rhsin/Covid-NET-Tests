6ffc021d7cab8938660be3244e102d0816b53853
rhsin <ryanhsin@ymail.com>
2020-10-28 15:07:54 -0700

    Use XUnit project template, include Moq

---
+++ AppUserControllerTests.cs
@@ -0,0 +1,29 @@
+using Covid.DTO;
+using Covid.Controllers;
+using System;
+using Xunit;
+
+namespace CovidTestProject
+{
+    public class AppUserControllerTests
+    {
+        [Fact]
+        public void TestApiResponse()
+        {
+            var appUserController = new
+        }
+
+
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
