c27abd7450c5a878691c3636b8b731671380d9b9
rhsin <ryanhsin@ymail.com>
2020-10-28 18:27:47 -0700

    Add sample test to check project reference

--- AppUserControllerTests.cs
+++ AppUserControllerTests.cs
@@ -1,19 +1,43 @@
+using Covid.Data;
 using Covid.DTO;
 using Covid.Controllers;
+using Covid.Models;
+using Covid.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace CovidTestProject
 {
-    public class AppUserControllerTests
+    public class AppUserControllerTests : ControllerBase
     {
         [Fact]
         public void TestApiResponse()
         {
-            var appUserController = new
+            var appUsers = new List<AppUserDTO>
+            {
+                new AppUserDTO { AccountId = 1, Name = "Ryan" },
+                new AppUserDTO { AccountId = 2, Name = "Alice" }
+            };
+
+            var apiResponse = Ok(new
+            {
+                Method = "Test",
+                Count = 2,
+                Data = appUsers
+            });
+
+            var appUserController = new AppUserController();
+
+            var result = appUserController.ApiResponse("Test", appUsers);
+
+            Assert.NotStrictEqual(apiResponse, result);
         }
-
-
 
         [Fact]
         public void PassingTest()
@@ -26,4 +50,21 @@ namespace CovidTestProject
             return x + y;
         }
     }
+
+    public class AppUserController : ControllerBase
+    {
+        public ActionResult<IEnumerable<AppUserDTO>> ApiResponse(string method,
+            IEnumerable<AppUserDTO> appUsers)
+        {
+            return Ok(new
+            {
+                Method = method,
+                Count = appUsers.Count(),
+                Data = appUsers
+            });
+        }
+    }
 }
+
+
+
